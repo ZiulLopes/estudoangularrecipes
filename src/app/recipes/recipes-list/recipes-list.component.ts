0f81e0fcfae7da34cfd857fb0fa1180363f577e0
Luiz Lopes <luizlopesziul@gmail.com>
2020-04-04 09:33:36 -0300

    first commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+
+  recipes = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
